9b8857a8163593bc634df2f95f50b4159ab16086
janakiraam <jnk266raam@gmail.com>
2021-12-06 14:41:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def main():
     st.title("UI Issue Prediction For Performance") 
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit UI Issue Prediction For Performance ML App </h2>
+    <h2 style="color:white;text-align:center;">UI Issue Prediction For Performance - ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
